c2e6c67fa4a4d15da60e7215d96e01256dcf6d8c
MrWilfis <pixelcreeper3000@gmail.com>
2025-04-01 09:46:02 +0500

    Changed some trades

--- src/main/java/net/mrwilfis/treasures_of_the_dead/event/ModEvents.java
+++ src/main/java/net/mrwilfis/treasures_of_the_dead/event/ModEvents.java
@@ -24,22 +24,29 @@ public class ModEvents {
             trades.get(1).add((entity, randomSource) -> new MerchantOffer(
                     new ItemStack(ModItems.FOUL_SKULL_ITEM.get(), 1),
                     new ItemStack(Items.EMERALD, randomSource.nextInt(2, 5+1)),
-                    4, 8, 0.0f));
+                    8, 8, 0.0f));
+
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(
+                    new ItemStack(ModItems.DISGRACED_SKULL_ITEM.get(), 1),
+                    new ItemStack(Items.EMERALD, randomSource.nextInt(7, 12+1)),
+                    6, 10, 0.0f));
 
             trades.get(2).add((entity, randomSource) -> new MerchantOffer(
-                    new ItemStack(ModItems.DISGRACED_SKULL_ITEM.get(), 1),
-                    new ItemStack(Items.EMERALD, randomSource.nextInt(7, 13+1)),
-                    4, 10, 0.0f));
+                    new ItemStack(ModItems.HATEFUL_SKULL_ITEM.get(), 1),
+                    new ItemStack(Items.EMERALD, randomSource.nextInt(13, 22+1)),
+                    4, 12, 0.0f));
 
             trades.get(3).add((entity, randomSource) -> new MerchantOffer(
-                    new ItemStack(ModItems.HATEFUL_SKULL_ITEM.get(), 1),
-                    new ItemStack(Items.EMERALD, randomSource.nextInt(16, 26+1)),
-                    4, 12, 0.0f));
+                    new ItemStack(ModItems.VILLAINOUS_SKULL_ITEM.get(), 1),
+                    new ItemStack(Items.EMERALD, randomSource.nextInt(23, 44+1)),
+                    4, 16, 0.0f));
 
             trades.get(4).add((entity, randomSource) -> new MerchantOffer(
-                    new ItemStack(ModItems.VILLAINOUS_SKULL_ITEM.get(), 1),
-                    new ItemStack(Items.EMERALD, randomSource.nextInt(28, 50+1)),
-                    4, 12, 0.0f));
+                    new ItemStack(Items.EMERALD, randomSource.nextInt(8, 16+1)),
+                    new ItemStack(ModItems.FOUL_SKULL_ITEM.get(), randomSource.nextInt(1, 2+1)),
+                    new ItemStack(ModItems.SKELETON_CREW_ASSIGNMENT.get(), 1),
+                    1, 16, 0.0f));
+
 
         }
     }
