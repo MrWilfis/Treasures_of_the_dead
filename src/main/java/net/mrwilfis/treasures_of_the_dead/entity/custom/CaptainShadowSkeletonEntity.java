bf36e494dadf7ca2700ee94b3dc9c993225a8fcb
MrWilfis <pixelcreeper3000@gmail.com>
2025-04-30 15:41:16 +0500

    the logic of class inheritance has been changed.

--- src/main/java/net/mrwilfis/treasures_of_the_dead/entity/custom/CaptainShadowSkeletonEntity.java
+++ src/main/java/net/mrwilfis/treasures_of_the_dead/entity/custom/CaptainShadowSkeletonEntity.java
@@ -3,18 +3,22 @@ package net.mrwilfis.treasures_of_the_dead.entity.custom;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -22,7 +26,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.mrwilfis.treasures_of_the_dead.entity.variant.CaptainShadowSkeletonVariant;
-import net.mrwilfis.treasures_of_the_dead.entity.variant.ShadowSkeletonVariant;
+import net.mrwilfis.treasures_of_the_dead.item.ModItems;
+import net.mrwilfis.treasures_of_the_dead.item.custom.AbstractPowderKegItem;
 import net.mrwilfis.treasures_of_the_dead.misc.TOTDBlockPos;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
@@ -31,17 +36,13 @@ import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.core.object.PlayState;
 
-public class CaptainShadowSkeletonEntity extends CaptainSkeletonEntity {
+public class CaptainShadowSkeletonEntity extends ShadowSkeletonEntity implements CaptainSkeletonInterface {
 
     private static final EntityDataAccessor<Float> DEATH_X = SynchedEntityData.defineId(CaptainShadowSkeletonEntity.class, EntityDataSerializers.FLOAT);
     private static final EntityDataAccessor<Float> DEATH_Z = SynchedEntityData.defineId(CaptainShadowSkeletonEntity.class, EntityDataSerializers.FLOAT);
     private static final EntityDataAccessor<Boolean> CAN_DROP_KEYS_AND_ORDERS = SynchedEntityData.defineId(CaptainShadowSkeletonEntity.class, EntityDataSerializers.BOOLEAN);
 
-    private static final EntityDataAccessor<Boolean> IS_SHADOW = SynchedEntityData.defineId(CaptainShadowSkeletonEntity.class, EntityDataSerializers.BOOLEAN);
-    private static final EntityDataAccessor<Integer> SHADOW_TIMER = SynchedEntityData.defineId(CaptainShadowSkeletonEntity.class, EntityDataSerializers.INT);
-    private boolean isShaking = false;
-    private int shakingTimer = 0;
-    private final int shadowTimer = 600;
+
 
     private static final EntityDataAccessor<Integer> DATA_ID_TYPE_VARIANT =
             SynchedEntityData.defineId(CaptainShadowSkeletonEntity.class, EntityDataSerializers.INT);
@@ -61,6 +62,9 @@ public class CaptainShadowSkeletonEntity extends CaptainSkeletonEntity {
 
         this.populateDefaultEquipmentSlots(randomsource);
         this.populateDefaultEquipmentEnchantments(randomsource, pDifficulty);
+
+        this.setCustomName(Component.literal(getRandomName(randomsource)));
+
         return super.finalizeSpawn(pLevel, pDifficulty, pReason, pSpawnData, pDataTag);
     }
 
@@ -76,130 +80,38 @@ public class CaptainShadowSkeletonEntity extends CaptainSkeletonEntity {
     }
 
     @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-        controllerRegistrar.add(new AnimationController<>(this, "controller1", 3, this::idleAndWalk2));
-        controllerRegistrar.add(new AnimationController<>(this, "controller2", 3, this::walkAndAttack2));
-    }
+    protected void dropCustomDeathLoot(DamageSource pSource, int pLooting, boolean pRecentlyHit) {
+        super.dropCustomDeathLoot(pSource, pLooting, pRecentlyHit);
 
-    private <E extends GeoAnimatable> PlayState walkAndAttack2(AnimationState<E> state) {
-
-        if (getShadowTimer() >= shadowTimer) {
-            //    System.out.println("SHAKING ANIMATION");
-            state.getController().setAnimation(SHAKING_FROM_LIGHT);
-            state.getController().setAnimationSpeed(1.0D);
-            return PlayState.CONTINUE;
-        }
-        else if (this.swinging) {
-            state.getController().stop();
-            state.getController().setAnimation(ATTACK1);
-            state.getController().setAnimationSpeed(1.0D);
-            return PlayState.CONTINUE;
-        }
-        else if (state.isMoving() && this.isAggressive()) {
-            state.getController().setAnimation(WALK_HANDS1);
-            state.getController().setAnimationSpeed(1.25D);
-            return PlayState.CONTINUE;
-        }
-        else if (state.isMoving() && !this.isAggressive()) {
-            state.getController().setAnimation(WALK_HANDS1);
-            state.getController().setAnimationSpeed(1.0D);
-            return PlayState.CONTINUE;
-        }
-        else if (!state.isMoving() && !this.swinging) {
-            return PlayState.STOP;
-        }
-        return PlayState.CONTINUE;
-    }
-
-    private <E extends GeoAnimatable> PlayState idleAndWalk2(AnimationState<E> state) {
-
-        if (getShadowTimer() >= shadowTimer) {
-            //    System.out.println("SHAKING ANIMATION");
-            state.getController().setAnimation(SHAKING_FROM_LIGHT);
-            state.getController().setAnimationSpeed(1.0D);
-            return PlayState.CONTINUE;
-        }
-        else if (state.isMoving() && this.isAggressive()) {
-            state.getController().setAnimation(WALK_BODY1);
-            state.getController().setAnimationSpeed(1.25D);
-            return PlayState.CONTINUE;
-        } else if (state.isMoving() && !this.isAggressive()) {
-            state.getController().setAnimation(WALK_BODY1);
-            state.getController().setAnimationSpeed(1.0D);
-            if ((double) random.nextFloat() < 0.5f) {
-                this.idleVariation = 1;
-            } else if ((double) random.nextFloat() < 1.0f) {
-                this.idleVariation = 2;
+        if (getCanDropKeysAndOrders()) {
+            double randomValue = (double) this.random.nextFloat();
+            if (randomValue < 0.4f) {
+                this.setDeathX((float) this.getX());
+                this.setDeathZ((float) this.getZ());
+                ItemStack stack = new ItemStack(ModItems.SKELETONS_ORDER.get());
+                stack.setTag(new CompoundTag());
+                stack.getTag().putFloat("DeathX", this.getDeathX());
+                stack.getTag().putFloat("DeathZ", this.getDeathZ());
+                ItemEntity itemEntity = this.spawnAtLocation(stack);
             }
-            return PlayState.CONTINUE;
-        } else if (!state.isMoving()) {
-            if (this.idleVariation == 1) {
-                state.getController().setAnimation(IDLE1);
-            } else if (this.idleVariation == 2) {
-                state.getController().setAnimation(IDLE2);
+            else if (randomValue < 0.6f)  {
+                this.setDeathX((float) this.getX());
+                this.setDeathZ((float) this.getZ());
+                ItemStack stack = new ItemStack(ModItems.SKELETON_CREW_ASSIGNMENT.get());
+                stack.setTag(new CompoundTag());
+                stack.getTag().putFloat("DeathX", this.getDeathX());
+                stack.getTag().putFloat("DeathZ", this.getDeathZ());
+                ItemEntity itemEntity = this.spawnAtLocation(stack);
             }
-            state.getController().setAnimationSpeed(1.0D);
-            return PlayState.CONTINUE;
+            else {
+                ItemEntity itemEntity = this.spawnAtLocation(ModItems.TREASURE_KEY.get());
+            }
         }
-
-        return PlayState.STOP;
     }
 
     @Override
-    public void tick() {
-        super.tick();
-
-        if (!this.level().isClientSide) {
-
-
-            BlockPos Pos = this.getLightPosition();
-            int i;
-            int j = this.level().getBrightness(LightLayer.BLOCK, Pos);
-            if (this.level().isNight()) {
-                i = 0;
-            } else {
-                i = this.level().getBrightness(LightLayer.SKY, Pos);
-            }
-            int brightness;
-            brightness = Math.max(i, j);
-
-
-            if (this.isShaking) {
-                setShadow(false);
-                setInvulnerable(false);
-                setNoAi(true);
-                this.shakingTimer--;
-                if (this.shakingTimer <= 0) {
-
-                    setShadowTimer(getShadowTimer() - 1);
-
-                    this.isShaking = false;
-                    setNoAi(false);
-                }
-            }
-
-
-            if (brightness >= 7)
-            {
-                if (getShadow() && !this.isShaking) {
-                    this.isShaking = true;
-                    this.shakingTimer = 60;
-                    this.playSound(SoundEvents.ZOMBIE_VILLAGER_CURE, 0.5f, 0.75f);
-                    this.playSound(SoundEvents.ALLAY_DEATH, 1.0f, 0.0f);
-                    setShadowTimer(shadowTimer);
-                }
-            }
-            else
-            {
-                if (!getShadow() && getShadowTimer() > 0 && !this.isShaking) {
-                    setShadowTimer(getShadowTimer()-1);
-                }
-                if (getShadowTimer() <= 0) {
-                    setShadow(true);
-                    setInvulnerable(true);
-                }
-            }
-        }
+    public int getExperienceReward() {
+        return  (int) (super.getExperienceReward() * 1.75);
     }
 
     @Override
@@ -209,30 +121,6 @@ public class CaptainShadowSkeletonEntity extends CaptainSkeletonEntity {
         } else {
             this.maybeWearEquipment(EquipmentSlot.MAINHAND, new ItemStack(Items.IRON_SWORD), pRandom, 0.9F);
         }
-    }
-
-    public BlockPos getLightPosition() {
-        BlockPos pos = TOTDBlockPos.fromVec3(this.position());
-        if (!level().getBlockState(pos).canOcclude()) {
-            return pos.above();
-        }
-        return pos;
-    }
-
-    public int getShadowTimer() {
-        return this.getEntityData().get(SHADOW_TIMER).intValue();
-    }
-
-    public void setShadowTimer(int i) {
-        this.getEntityData().set(SHADOW_TIMER, i);
-    }
-
-    public boolean getShadow() {
-        return this.getEntityData().get(IS_SHADOW).booleanValue();
-    }
-
-    public void setShadow(boolean b) {
-        this.getEntityData().set(IS_SHADOW, b);
     }
 
     public CaptainShadowSkeletonVariant getCaptainShadowVariant() {
@@ -247,12 +135,34 @@ public class CaptainShadowSkeletonEntity extends CaptainSkeletonEntity {
         this.entityData.set(DATA_ID_TYPE_VARIANT, variant.getId() & 255);
     }
 
+    public boolean getCanDropKeysAndOrders() {
+        return this.getEntityData().get(CAN_DROP_KEYS_AND_ORDERS).booleanValue();
+    }
+
+    public void setCanDropKeysAndOrders(boolean b) {
+        this.getEntityData().set(CAN_DROP_KEYS_AND_ORDERS, b);
+    }
+
+    public float getDeathX() {
+        return this.getEntityData().get(DEATH_X).floatValue();
+    }
+
+    public void setDeathX(float var) {
+        this.getEntityData().set(DEATH_X, var);
+    }
+
+    public float getDeathZ() {
+        return this.getEntityData().get(DEATH_Z).floatValue();
+    }
+
+    public void setDeathZ(float var) {
+        this.getEntityData().set(DEATH_Z, var);
+    }
+
     @Override
     public void readAdditionalSaveData(CompoundTag tag) {
         super.readAdditionalSaveData(tag);
         this.entityData.set(DATA_ID_TYPE_VARIANT, tag.getInt("Variant"));
-        this.setShadowTimer(tag.getInt("ShadowTimer"));
-        this.setShadow(tag.getBoolean("IsShadow"));
 
         this.setDeathX(tag.getFloat("DeathX"));
         this.setDeathZ(tag.getFloat("DeathZ"));
@@ -263,8 +173,6 @@ public class CaptainShadowSkeletonEntity extends CaptainSkeletonEntity {
     public void addAdditionalSaveData(CompoundTag tag) {
         super.addAdditionalSaveData(tag);
         tag.putInt("Variant", this.getTypeVariant());
-        tag.putInt("ShadowTimer", this.getShadowTimer());
-        tag.putBoolean("IsShadow", this.getShadow());
 
         tag.putFloat("DeathX", this.getDeathX());
         tag.putFloat("DeathZ", this.getDeathZ());
@@ -275,8 +183,6 @@ public class CaptainShadowSkeletonEntity extends CaptainSkeletonEntity {
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(DATA_ID_TYPE_VARIANT, 0);
-        this.entityData.define(SHADOW_TIMER, 0);
-        this.getEntityData().define(IS_SHADOW, true);
 
         this.getEntityData().define(DEATH_X, 0.0f);
         this.getEntityData().define(DEATH_Z, 0.0f);
