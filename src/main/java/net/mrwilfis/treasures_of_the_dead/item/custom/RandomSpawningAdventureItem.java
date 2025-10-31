31ce74fa318b32c5a380ff026fe71e337a1a6ef1
MrWilfis <pixelcreeper3000@gmail.com>
2025-10-31 12:03:48 +0500

    new skeleton crew camp mechanics

--- src/main/java/net/mrwilfis/treasures_of_the_dead/item/custom/RandomSpawningAdventureItem.java
+++ src/main/java/net/mrwilfis/treasures_of_the_dead/item/custom/RandomSpawningAdventureItem.java
@@ -19,6 +19,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MapItem;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.Heightmap;
@@ -35,6 +36,7 @@ import net.mrwilfis.treasures_of_the_dead.entity.variant.*;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 
+import java.util.List;
 import java.util.Random;
 
 public class RandomSpawningAdventureItem extends Item {
@@ -65,9 +67,14 @@ public class RandomSpawningAdventureItem extends Item {
         double Y = player.getY();
         double Z = player.getZ();
 
+        int difficulty = 0;
+
         if (stack.get(ModDataComponents.CAPTAIN_SKELETON_DEATH_X) != null && stack.get(ModDataComponents.CAPTAIN_SKELETON_DEATH_Z) != null) {
             X = stack.get(ModDataComponents.CAPTAIN_SKELETON_DEATH_X).floatValue();
             Z = stack.get(ModDataComponents.CAPTAIN_SKELETON_DEATH_Z).floatValue();
+        }
+        if (stack.getComponents().get(ModDataComponents.DIFFICULTY) != null) {
+             difficulty = stack.getComponents().get(ModDataComponents.DIFFICULTY).intValue();
         }
 
         X = rand.nextDouble(X-range, X+range);
@@ -110,7 +117,9 @@ public class RandomSpawningAdventureItem extends Item {
                     pos = new BlockPos((int)X, (int)Y, (int)Z);
                 }
 
-                summonSkeletonCrew(X, Y, Z, player, level, random);
+                //summonSkeletonCrew(X, Y, Z, player, level, random);
+
+                summonSkeletonCrewCamp(X, Y, Z, player, level, random, difficulty);
             }
 
 
@@ -119,6 +128,15 @@ public class RandomSpawningAdventureItem extends Item {
         player.awardStat(Stats.ITEM_USED.get(this));
 
         return super.use(level, player, pUsedHand);
+    }
+
+    private void summonSkeletonCrewCamp(double x, double y, double z, Player player, Level level, RandomSource random, int difficulty) {
+        if (!level.isClientSide) {
+            giveMap(x, y, z, player, level, random, "filled_map.treasures_of_the_dead.skeleton_crew", "skeleton_crew");
+            SkeletonCrewCamp skeletonCrewCamp = new SkeletonCrewCamp(ModEntities.SKELETON_CREW_CAMP.get(), level, difficulty, 3);
+            skeletonCrewCamp.moveTo(x, y, z);
+            level.addFreshEntity(skeletonCrewCamp);
+        }
     }
 
     private void summonSkeletonCrew(double x, double y, double z, Player player, Level level, RandomSource random) {
@@ -132,7 +150,7 @@ public class RandomSpawningAdventureItem extends Item {
 
             double randomValue = (double) random.nextFloat();
 
-            if (randomValue < 0.4) {
+            if (randomValue < 0.3) {
                 for (int i = 0; i < captainsAmount; i++) {
 
                     CaptainSkeletonEntity captain = new CaptainSkeletonEntity(ModEntities.CAPTAIN_SKELETON.get(), level);
@@ -144,6 +162,8 @@ public class RandomSpawningAdventureItem extends Item {
                     CaptainSkeletonVariant variant = Util.getRandom(CaptainSkeletonVariant.values(), random);
                     captain.setVariant(variant);
 
+                    captain.populateDefaultEquipmentSlots(random);
+
                     x = x + random.nextInt(-2, 2+1);
                     z = z + random.nextInt(-2, 2+1);
                     y = level.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
@@ -153,6 +173,39 @@ public class RandomSpawningAdventureItem extends Item {
                     newSkeleton(x, y, z, level, random, pirate, rand);
                     TOTDSkeletonVariant variant = Util.getRandom(TOTDSkeletonVariant.values(), random);
                     pirate.setVariant(variant);
+
+                    pirate.populateDefaultEquipmentSlots(random);
+
+                    x = x + random.nextInt(-2, 2+1);
+                    z = z + random.nextInt(-2, 2+1);
+                    y = level.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
+                }
+            }
+            else if (randomValue < 0.5) {
+                for (int i = 0; i < captainsAmount; i++) {
+
+                    CaptainGoldenSkeletonEntity captain = new CaptainGoldenSkeletonEntity(ModEntities.CAPTAIN_GOLDEN_SKELETON.get(), level);
+                    newSkeleton(x, y, z, level, random, captain, rand);
+
+                    captain.setCanDropKeysAndOrders(false);
+                    captain.setCustomName(Component.literal(captain.getRandomName(random)));
+
+                    CaptainGoldenSkeletonVariant variant = Util.getRandom(CaptainGoldenSkeletonVariant.values(), random);
+                    captain.setVariant(variant);
+
+                    captain.populateDefaultEquipmentSlots(random);
+
+                    x = x + random.nextInt(-2, 2+1);
+                    z = z + random.nextInt(-2, 2+1);
+                    y = level.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
+                }
+                for (int i = 0; i < (captainsAmount / 2) + 3; i++) {
+                    GoldenSkeletonEntity pirate = new GoldenSkeletonEntity(ModEntities.GOLDEN_SKELETON.get(), level);
+                    newSkeleton(x, y, z, level, random, pirate, rand);
+                    GoldenSkeletonVariant variant = Util.getRandom(GoldenSkeletonVariant.values(), random);
+                    pirate.setVariant(variant);
+
+                    pirate.populateDefaultEquipmentSlots(random);
 
                     x = x + random.nextInt(-2, 2+1);
                     z = z + random.nextInt(-2, 2+1);
@@ -170,6 +223,8 @@ public class RandomSpawningAdventureItem extends Item {
                     CaptainShadowSkeletonVariant variant = Util.getRandom(CaptainShadowSkeletonVariant.values(), random);
                     captain.setVariant(variant);
 
+                    captain.populateDefaultEquipmentSlots(random);
+
                     x = x + random.nextInt(-2, 2+1);
                     z = z + random.nextInt(-2, 2+1);
                     y = level.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
@@ -180,12 +235,14 @@ public class RandomSpawningAdventureItem extends Item {
                     ShadowSkeletonVariant variant = Util.getRandom(ShadowSkeletonVariant.values(), random);
                     pirate.setVariant(variant);
 
+                    pirate.populateDefaultEquipmentSlots(random);
+
                     x = x + random.nextInt(-2, 2+1);
                     z = z + random.nextInt(-2, 2+1);
                     y = level.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
                 }
 
-            } else {
+            } else if (randomValue < 1.0){
                 for (int i = 0; i < captainsAmount; i++) {
 
                     CaptainBloomingSkeletonEntity captain = new CaptainBloomingSkeletonEntity(ModEntities.CAPTAIN_BLOOMING_SKELETON.get(), level);
@@ -197,6 +254,8 @@ public class RandomSpawningAdventureItem extends Item {
                     CaptainBloomingSkeletonVariant variant = Util.getRandom(CaptainBloomingSkeletonVariant.values(), random);
                     captain.setVariant(variant);
 
+                    captain.populateDefaultEquipmentSlots(random);
+
                     x = x + random.nextInt(-2, 2+1);
                     z = z + random.nextInt(-2, 2+1);
                     y = level.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
@@ -206,6 +265,8 @@ public class RandomSpawningAdventureItem extends Item {
                     newSkeleton(x, y, z, level, random, pirate, rand);
                     BloomingSkeletonVariant variant = Util.getRandom(BloomingSkeletonVariant.values(), random);
                     pirate.setVariant(variant);
+
+                    pirate.populateDefaultEquipmentSlots(random);
 
                     x = x + random.nextInt(-2, 2+1);
                     z = z + random.nextInt(-2, 2+1);
@@ -220,8 +281,8 @@ public class RandomSpawningAdventureItem extends Item {
         skeleton.addTag("TOTD_Rotate");
         skeleton.setPersistenceRequired();
         level.addFreshEntity(skeleton);
-        skeleton.populateDefaultEquipmentSlots(random);
-    //    skeleton.setIsSpawning(true);
+    //    skeleton.populateDefaultEquipmentSlots(random);
+        skeleton.setIsSpawning(true);
     }
 
     private void summonTreasure(double x, double y, double z, Player player, Level level, RandomSource random) {
@@ -303,6 +364,19 @@ public class RandomSpawningAdventureItem extends Item {
             y = treasure.getBlockY();
             pos1 = new BlockPos(x, y, z);
             pos2 = new BlockPos(x, y-1, z);
+        }
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        int difficulty = 0;
+        if (stack.getComponents().get(ModDataComponents.DIFFICULTY) != null) {
+            difficulty = stack.getComponents().get(ModDataComponents.DIFFICULTY).intValue();
+        }
+        if (this.taskType.equals("skeleton_crew")) {
+            tooltipComponents.add(Component.translatable("tooltip.treasures_of_the_dead.quest_difficulty.tooltip", difficulty));
+            //tooltipComponents.add(Component.literal("Сложность задания: " + difficulty));
         }
     }
 }
