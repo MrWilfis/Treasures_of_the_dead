0e77b0bc346fab5f432c6e4b078fefd27ed851b9
MrWilfis <pixelcreeper3000@gmail.com>
2026-02-25 16:22:25 +0500

    DataComponentType<Float> NAME -> DeferredHolder<DataComponentType<?>, DataComponentType<Float>> X and others too; added new Components

--- src/main/java/net/mrwilfis/treasures_of_the_dead/common/ModDataComponents.java
+++ src/main/java/net/mrwilfis/treasures_of_the_dead/common/ModDataComponents.java
@@ -1,56 +1,62 @@
 package net.mrwilfis.treasures_of_the_dead.common;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.mrwilfis.treasures_of_the_dead.Treasures_of_the_dead;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.UUID;
 import java.util.function.UnaryOperator;
 
 public class ModDataComponents {
-    private static final DeferredRegister.DataComponents DATA_COMPONENTS = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, Treasures_of_the_dead.MOD_ID);
+    private static final DeferredRegister.DataComponents DATA_COMPONENTS =
+            DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, Treasures_of_the_dead.MOD_ID);
 
-    public static final DataComponentType<Float> CAPTAIN_SKELETON_DEATH_X = register(
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Float>> CAPTAIN_SKELETON_DEATH_X = register(
             "death_x",
-            builder -> builder.persistent(Codec.FLOAT).networkSynchronized(ByteBufCodecs.FLOAT)
-    );
-    public static final DataComponentType<Float> CAPTAIN_SKELETON_DEATH_Z = register(
+            builder -> builder.persistent(Codec.FLOAT).networkSynchronized(ByteBufCodecs.FLOAT));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Float>> CAPTAIN_SKELETON_DEATH_Z = register(
             "death_z",
-            builder -> builder.persistent(Codec.FLOAT).networkSynchronized(ByteBufCodecs.FLOAT)
-    );
-    public static final DataComponentType<Integer> DIFFICULTY = register(
+            builder -> builder.persistent(Codec.FLOAT).networkSynchronized(ByteBufCodecs.FLOAT));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> DIFFICULTY = register(
             "difficulty",
-            builder -> builder.persistent(Codec.INT).networkSynchronized(ByteBufCodecs.INT)
-    );
-    public static final DataComponentType<Boolean> TREASURE_CHEST_IS_ROBBED = register(
+            builder -> builder.persistent(Codec.INT).networkSynchronized(ByteBufCodecs.INT));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Boolean>> TREASURE_CHEST_IS_ROBBED = register(
             "treasure_chest_is_robbed",
-            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL)
-    );
-    public static final DataComponentType<Boolean> TREASURE_CHEST_IS_OPEN = register(
+            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Boolean>> TREASURE_CHEST_IS_OPEN = register(
             "treasure_chest_is_open",
-            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL)
-    );
-    public static final DataComponentType<Boolean> KEG_IS_GOING_TO_BLOW_UP = register(
+            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Boolean>> KEG_IS_GOING_TO_BLOW_UP = register(
             "keg_is_going_to_blow_up",
-            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL)
-    );
-    public static final DataComponentType<Integer> KEG_PREPARE_TO_BLOW_UP = register(
+            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> KEG_PREPARE_TO_BLOW_UP = register(
             "keg_prepare_to_blow_up",
-            builder -> builder.persistent(Codec.INT).networkSynchronized(ByteBufCodecs.INT)
-    );
-    public static final DataComponentType<Boolean> GUN_IS_CHARGED = register(
+            builder -> builder.persistent(Codec.INT).networkSynchronized(ByteBufCodecs.INT));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Boolean>> GUN_IS_CHARGED = register(
             "is_charged",
-            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL)
-    );
+            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<BlockPos>> SEA_FORTRESS_COORDINATES = register(
+            "sea_fortress_coordinates",
+            builder -> builder.persistent(BlockPos.CODEC));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Float>> LOOT_VALUE = register(
+            "loot_value",
+            builder -> builder.persistent(Codec.FLOAT).networkSynchronized(ByteBufCodecs.FLOAT));
 
+//    private static <T> DataComponentType<T> register(String name, UnaryOperator<DataComponentType.Builder<T>> builder) {
+//        DataComponentType<T> type = builder.apply(DataComponentType.builder()).build();
+//        DATA_COMPONENTS.register(name, () -> type);
+//        return type;
+//    }
 
-    private static <T> DataComponentType<T> register(String name, UnaryOperator<DataComponentType.Builder<T>> builder) {
-        DataComponentType<T> type = builder.apply(DataComponentType.builder()).build();
-        DATA_COMPONENTS.register(name, () -> type);
-        return type;
+    private static <T>DeferredHolder<DataComponentType<?>, DataComponentType<T>> register(String name,
+                                                                                           UnaryOperator<DataComponentType.Builder<T>>builderUnaryOperator) {
+        return DATA_COMPONENTS.register(name, () -> builderUnaryOperator.apply(DataComponentType.builder()).build());
     }
 
     public static void register(IEventBus event) {
