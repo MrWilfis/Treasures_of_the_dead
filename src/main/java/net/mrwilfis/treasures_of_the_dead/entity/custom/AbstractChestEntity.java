7eb077b949e862956fcab54324eeb11b2baa5ede
MrWilfis <pixelcreeper3000@gmail.com>
2026-03-09 20:26:18 +0500

    moved method isTopSlab() extends Item -> extends AnyTreasureItem

--- src/main/java/net/mrwilfis/treasures_of_the_dead/entity/custom/AbstractChestEntity.java
+++ src/main/java/net/mrwilfis/treasures_of_the_dead/entity/custom/AbstractChestEntity.java
@@ -1,15 +1,10 @@
 package net.mrwilfis.treasures_of_the_dead.entity.custom;
 
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.mrwilfis.treasures_of_the_dead.entity.custom.chestVariants.TreasureChestEntity;
-import org.jetbrains.annotations.NotNull;
 
 public class AbstractChestEntity extends AnyTreasureClass {
 
